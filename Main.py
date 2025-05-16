27144fd9917583b1201d8fc78dceb070cff54fa5
Esteban Dutrannois <esteban.dutrannois@outlook.com>
2025-05-16 15:29:14 +0200

    feat: print je suis sur la branche main

--- Main.py
+++ Main.py
@@ -1,3 +1,8 @@
 print("coucou")
 # Je viens de repasser sur ma branche main, nous allons maintenant merge
 # Petit oublie ne pas oublier de faire un git fetch --all dans la console pour récupérer les branches distantes
+
+
+# Le cas des merges conflict : je code un truc dans main
+
+print("Je suis sur la branche main")
