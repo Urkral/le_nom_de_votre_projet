54df1aed00b56a534d136f52fb898164e2b681b0
Esteban Dutrannois <esteban.dutrannois@outlook.com>
2025-05-16 16:53:17 +0200

    Feat: Last commentaire

--- Main.py
+++ Main.py
@@ -12,3 +12,16 @@ print("Je suis sur la branche main")
 # Je repasse sur la branche personnage et je code un autre trucs
 
 print("Je suis sur la branche personnage")
+
+
+# Voilà donc les bases de github
+# Voilà comment ajouter quelqu'un à un projet
+
+# Pour les bonnes pratiques de git:
+# - Toujours faire un git pull avant de commencer à coder (Vous aurez une icône ici pour git pull ou vous pouvez cliquer en bas a gauche ici)
+# Pour le nom des commits: 
+# Toujours commencer votre commit par un feat (feature) nouvelle fonctionnalité, un fix correction de bug, un refactor (refactorisation) pour les changements de code qui n'impactent pas le fonctionnement du programme, un style (style) pour les changements de style de code (indentation, espaces, etc.), un test (test) pour les ajouts de tests, un chore (chore) pour les changements de configuration ou de build.
+# Copilot à tous compris xD
+# Et pour finir on évite de faire ces merge 1h avant de rendre le projet
+# On se parler pour éviter les merges conflicts
+# Voilà donc les bases de github
