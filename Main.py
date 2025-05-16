aba6dfcc793f2b2545e0d0a96bc84894fce134c7
Esteban Dutrannois <esteban.dutrannois@outlook.com>
2025-05-16 15:28:29 +0200

    Feat: commentaire + merge

--- Main.py
+++ Main.py
@@ -1,1 +1,3 @@
 print("coucou")
+# Je viens de repasser sur ma branche main, nous allons maintenant merge
+# Petit oublie ne pas oublier de faire un git fetch --all dans la console pour récupérer les branches distantes
